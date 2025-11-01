77f799b5c6b50ddff9a4a00ff6cabffff8ddd878
legit <victornunez123411@gmail.com>
2025-11-01 16:11:36 -0400

    Add files via upload

---
+++ Setup.py
@@ -0,0 +1,41 @@
+# Copyright (c) Legit
+# See the file 'LICENSE' for copying permission
+# ----------------------------------------------------------------------------------------------------------------------------------------------------------|
+# EN: 
+#     - Do not touch or modify the code below. If there is an error, please contact the owner, but under no circumstances should you touch the code.
+#     - Do not resell this tool, do not credit it to yours.
+# FR: 
+#     - Ne pas toucher ni modifier le code ci-dessous. En cas d'erreur, veuillez contacter le propriétaire, mais en aucun cas vous ne devez toucher au code.
+#     - Ne revendez pas ce tool, ne le créditez pas au vôtre.
+
+
+try:
+    import sys
+    import os
+
+    def OpenSites():
+        try:
+            import webbrowser
+            from Program.Config.Config import telegram, gunslol
+            webbrowser.open(f'}')
+            webbrowser.open(f'https://{lol}')
+        except: pass
+
+    if sys.platform.startswith("win"):
+        os.system("cls")
+        print("Installing the python modules required for the LegitPanel Tool:\n")
+        os.system("python -m pip install --upgrade pip")
+        os.system("python -m pip install -r requirements.txt")
+        OpenSites()
+        os.system("python RedTiger.py")
+
+    elif sys.platform.startswith("linux"):
+        os.system("clear")
+        print("Installing the python modules required for the LegitPanel Tool:\n")
+        os.system("pip3 install --upgrade pip")
+        os.system("pip3 install -r requirements.txt")
+        OpenSites()
+        os.system("python3 LegitPanel.py")
+
+except Exception as e:
+    input(e)
